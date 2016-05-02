5db1daa131eec3a9b5aa34e10c80a73c12090d15
Kevin Sapper <mail@kevinsapper.de>
2016-05-02 17:39:50 +0200

    use own deploy script

---
+++ ci_deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+if [ "$TRAVIS_TAG" != "" ]; then
+    if [ "$BUILD_TYPE" == "android" ]; then
+        export CZMQ_DEPLOYMENT=builds/android/prefix/arm-linux-androideabi-4.9/lib/libczmq.a
+    else
+        export CZMQ_DEPLOYMENT=""
+    fi
+fi
