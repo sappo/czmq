04f9346b770072479f50e90eefb40d4efb066b02
Kevin Sapper <mail@kevinsapper.de>
2016-05-03 09:07:01 +0200

    Yeah! Deploy multiple files

--- ci_deploy.sh
+++ ci_deploy.sh
@@ -7,7 +7,7 @@ if [ "$TRAVIS_TAG" != "" ]; then
     if [ "$BUILD_TYPE" == "android" ]; then
         export CZMQ_DEPLOYMENT=builds/android/prefix/arm-linux-androideabi-4.9/lib/libczmq.a
     else
-        export CZMQ_DEPLOYMENT="README.md"
+        export CZMQ_DEPLOYMENT="README.md, README.txt"
     fi
 fi
 export
