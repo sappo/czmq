f3f8df928cd3c4c4996ac9c43878d993ec162a59
Kevin Sapper <mail@kevinsapper.de>
2016-05-03 10:00:39 +0200

    Skip deployments

--- ci_deploy.sh
+++ ci_deploy.sh
@@ -3,11 +3,10 @@
 set -x
 set -e
 
-if [ "$TRAVIS_TAG" != "" ]; then
-    if [ "$BUILD_TYPE" == "android" ]; then
-        export CZMQ_DEPLOYMENT=builds/android/prefix/arm-linux-androideabi-4.9/lib/libczmq.a
-    else
-        export CZMQ_DEPLOYMENT="[README.md, README.txt]"
-    fi
+if [ "$BUILD_TYPE" == "default" ]; then
+    export CZMQ_DEPLOYMENT=README.*
+if [ "$BUILD_TYPE" == "android" ]; then
+    export CZMQ_DEPLOYMENT=builds/android/prefix/arm-linux-androideabi-4.9/lib/libczmq.a
+else
+    export CZMQ_DEPLOYMENT=""
 fi
-export
