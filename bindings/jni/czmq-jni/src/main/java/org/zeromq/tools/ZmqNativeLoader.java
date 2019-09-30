a63cd073bf908a228cc0cdd8e1197da7de6c5426
Kevin Sapper <mail@kevinsapper.de>
2019-09-30 10:07:52 +0200

    Give the option to load a library mapping from a mapping properties file

--- bindings/jni/czmq-jni/src/main/java/org/zeromq/tools/ZmqNativeLoader.java
+++ bindings/jni/czmq-jni/src/main/java/org/zeromq/tools/ZmqNativeLoader.java
@@ -1,15 +1,38 @@
 package org.zeromq.tools;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Properties;
+import org.scijava.nativelib.NativeLibraryUtil;
 import org.scijava.nativelib.NativeLoader;
 
 public class ZmqNativeLoader {
 
     private static final Set<String> loadedLibraries = new HashSet<>();
 
+    private static final Properties properties;
+
+    static {
+        properties = new Properties();
+        try {
+            final String architecture = NativeLibraryUtil.getArchitecture().name().toLowerCase();
+            final InputStream libraryMappingInputStream =
+                ZmqNativeLoader.class.getClassLoader().getResourceAsStream(architecture + "/mapping.properties");
+            if (libraryMappingInputStream != null) {
+                properties.load(libraryMappingInputStream);
+            }
+        } catch (IOException e) {
+            System.err.println("[ERROR] " + e.getMessage());
+        }
+    }
+
     public static void loadLibrary(String libname, boolean optional) {
+        final String libnameMapping = properties.getProperty(libname);
+        if (libnameMapping != null) {
+            libname = libnameMapping;
+        }
         if (!loadedLibraries.contains(libname)) {
             try {
                 NativeLoader.loadLibrary(libname);
